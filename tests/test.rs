cad75011d4ad97fc8dae4333fee365397feb359c
Kenny Kerr <kenny@kennykerr.ca>
2022-11-14 12:51:57 -0600

    import_name_type

--- tests/test.rs
+++ tests/test.rs
@@ -1,7 +1,7 @@
 #![feature(raw_dylib)]
 #![feature(native_link_modifiers_verbatim)]
 
-#[link(name = "kernel32.dll", kind = "raw-dylib", modifiers = "+verbatim")]
+#[link(name = "kernel32.dll", kind = "raw-dylib", modifiers = "+verbatim", import_name_type = "undecorated")]
 extern "stdcall" {
     fn LoadLibraryA(name: *const u8) -> isize;
 }
