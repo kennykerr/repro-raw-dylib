fd1cde7d32664120d30d112f2e01e391e41a25b5
Kenny Kerr <kenny@kennykerr.ca>
2022-11-14 12:07:22 -0600

    test

---
+++ tests/test.rs
@@ -0,0 +1,12 @@
+#![feature(raw_dylib)]
+#![feature(native_link_modifiers_verbatim)]
+
+#[link(name = "kernel32.dll", kind = "raw-dylib", modifiers = "+verbatim")]
+extern "stdcall" {
+    fn LoadLibraryA(name: *const u8) -> isize;
+}
+
+#[test]
+fn test() {
+    assert_ne!(0, unsafe { LoadLibraryA("kernel32.dll\0".as_ptr()) });
+}
